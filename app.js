76b11c3b12315ca820e81c2c9d38cd3b9430e427
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-12-09 18:34:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,62 @@ const inputElement = document.getElementById('title');
 const createBtn = document.getElementById('create');
 const listElement = document.getElementById('list');
 
-const notes = ['записать блок про массивы', 'рассказать теорию объектов'];
+// const notes = ['записать блок про массивы', 'рассказать теорию объектов'];
+
+// function render() {
+//   // for(i =0; i < notes.length; i++) {
+//   //   listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]));
+//   // }
+
+//   // for (let note of notes) {
+//   //   listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note));
+//   // }
+// }
+
+// render();
+
+// createBtn.onclick = function () {
+//   if (inputElement.value.length === 0) {
+//     return;
+//   }
+//   listElement.insertAdjacentHTML(
+//     'beforeend',
+//     getNoteTemplate(inputElement.value)
+//   );
+//   inputElement.value = '';
+// };
+
+// function getNoteTemplate(title) {
+//   return `
+//         <li class="list-group-item d-flex justify-content-between align-items-center">
+//             <span>${title}</span>
+//             <span>
+//             <span class="btn btn-small btn-success">&check;</span>
+//             <span class="btn btn-small btn-danger">&times;</span>
+//             </span>
+//         </li>
+//     `;
+// }
+
+const notes = [
+  {
+    title: 'записать блок про массивы',
+    completed: false,
+  },
+  {
+    title: 'рассказать теорию объектов',
+    completed: true,
+  },
+];
 
 function render() {
-  listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[0]));
-  listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[1]));
+  // for(i =0; i < notes.length; i++) {
+  //   listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]));
+  // }
+
+  for (let note of notes) {
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note));
+  }
 }
 
 render();
@@ -22,10 +73,10 @@ createBtn.onclick = function () {
   inputElement.value = '';
 };
 
-function getNoteTemplate(title) {
+function getNoteTemplate(note) {
   return `
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${title}</span>
+            <span>${note.title}</span>
             <span>
             <span class="btn btn-small btn-success">&check;</span>
             <span class="btn btn-small btn-danger">&times;</span>
