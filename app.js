0b40a26ea3295bd2e8bbe18bc7a6ba3d6fa6e1a8
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-12-09 09:51:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,21 +2,34 @@ const inputElement = document.getElementById('title');
 const createBtn = document.getElementById('create');
 const listElement = document.getElementById('list');
 
+const notes = ['записать блок про массивы', 'рассказать теорию объектов'];
+
+function render() {
+  listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[0]));
+  listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[1]));
+}
+
+render();
+
 createBtn.onclick = function () {
   if (inputElement.value.length === 0) {
     return;
   }
   listElement.insertAdjacentHTML(
     'beforeend',
-    `
+    getNoteTemplate(inputElement.value)
+  );
+  inputElement.value = '';
+};
+
+function getNoteTemplate(title) {
+  return `
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${inputElement.value}</span>
+            <span>${title}</span>
             <span>
             <span class="btn btn-small btn-success">&check;</span>
             <span class="btn btn-small btn-danger">&times;</span>
             </span>
         </li>
-    `
-  );
-  inputElement.value = '';
-};
+    `;
+}
