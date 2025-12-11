bf838480ac5bb44d9ec4cc74c9d230951933d209
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-12-11 13:11:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const notes = [
 function render() {
   listElement.innerHTML = '';
   for (i = 0; i < notes.length; i++) {
-    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]), i);
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i));
   }
 
   // for (let note of notes) {
@@ -77,8 +77,19 @@ createBtn.onclick = function () {
 };
 
 listElement.onclick = function (event) {
-  console.log(event.target);
-}
+  if (event.target.dataset.index) {
+    const index = parseInt(event.target.dataset.index);
+    const type = event.target.dataset.type;
+
+    if (type === 'toggle') {
+      notes[index].completed = !notes[index].completed;
+    } else if (type === 'remove') {
+      notes.splice(index, 1);
+    }
+
+    render();
+  }
+};
 
 function getNoteTemplate(note, index) {
   return `
@@ -87,9 +98,10 @@ function getNoteTemplate(note, index) {
               note.completed ? 'text-decoration-line-through' : ''
             }">${note.title}</span>
             <span>
-            <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'
-            }" data-index="${index}" data-type="toggle">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
+              <span class="btn btn-small btn-${
+                note.completed ? 'warning' : 'success'
+              }" data-index="${index}" data-type="toggle">&check;</span>
+              <span class="btn btn-small btn-danger" data-type="remove" data-index="${index}">&times;</span>
             </span>
         </li>
     `;
