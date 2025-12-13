9f81bef0945f11bb781adf6914930440ef0ddba6
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-12-13 17:12:00 +0200

    Update array-theory.js

--- array-theory.js
+++ array-theory.js
@@ -1,3 +1,9 @@
 const names = ['Елена', 'Максим', 'Олег', 'Ксения'];
 
-names.push('Алёна');
+// names.push('Алёна');
+// names.unshift('Алёна');
+
+const name = names.pop()
+
+
+console.log(names, name);
